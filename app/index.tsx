5e14b6bfb933cb47806d9a08289de8f3ec0f4260
bruno <bruno.cavalcante25@etec.sp.gov.br>
2025-04-03 21:34:41 -0300

    useStates aplicado

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { Button, Text, View } from "react-native";
+
+export default function HomeScreen() {
+    const[count,setCount] = useState(0);
+
+    return (
+        <View>
+            <Text>Bem-vindo ao meu app</Text>
+            <Text>Contador: {count}</Text>
+            <Button title="Aumentar" onPress={() => {setCount(count + 1)}}/>
+                <Button title="Diminuir" onPress={() => {setCount(count - 1)}}/>
+                    <Button title="Resetar" onPress={() => {setCount(0)}}/>
+        </View>
+    );
+}
